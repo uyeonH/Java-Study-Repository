53d7c8198b0aaae9dd12944260aff5f486f381bf
uyeonH <yooyeon0508@gmail.com>
2020-07-21 19:51:00 +0900

    add files

---
+++ practice02/bin/src/main/java/practice02/Prob02.java
@@ -0,0 +1,14 @@
+package practice02;
+
+import java.util.Scanner;
+
+public class Prob02 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner( System.in );
+		int intArray[] = new int[5];
+		
+		double sum = 0;
+
+		
+	}
+}
