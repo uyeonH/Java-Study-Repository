ab5e42f8d17f74ad979e892f1bb030740e7d703d
uyeonH <yooyeon0508@gmail.com>
2020-07-22 09:42:31 +0900

    update chapter02,03

---
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -0,0 +1,54 @@
+package chapter03;
+public class Goods {
+	private String name;
+	private int price;
+	private int countStock;
+	private int countSold;
+	
+	public static int countofGoods = 0;
+	
+	public Goods() {
+		countofGoods++; 
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		if(price < 0) {
+			price = 0;
+		}
+		this.price = price;
+	}
+	public int getCountStock() {
+		return countStock;
+	}
+	public void setCountStock(int countStock) {
+		this.countStock = countStock;
+	}
+	public int getCountSold() {
+		return countSold;
+	}
+	public void setCountSold(int countSold) {
+		this.countSold = countSold;
+	}
+	
+	public void showInfo() {
+		System.out.println(
+			"상품이름 :" + name + 
+			",가격:" + price +
+			",재고개수:" + countStock + 
+			",팔린 개수:" + countSold
+		);
+	}
+	
+	public int calculateDiscountPrice(double percentage) {
+		return (int)(price * percentage);
+	}
+}
---
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -0,0 +1,20 @@
+package chapter03;
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods camera = new Goods();
+		
+		camera.setName("nikon");
+		camera.setPrice(200000);
+		camera.setCountSold(20);
+		camera.setCountStock(30);
+		
+		Goods camera1 = new Goods();
+		Goods camera2 = new Goods();
+		Goods camera3 = new Goods();
+		
+		camera.showInfo();
+		System.out.println(camera.calculateDiscountPrice(0.5));
+	}
+
+}
